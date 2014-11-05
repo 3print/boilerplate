c385e5a003c3195b46db6a25dba49269e05ad386
abe33 <cedric.nehemie@gmail.com>
2014-11-05 14:35:56 +0100

    :bug: Fix missing I18n custom methods

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -2,6 +2,10 @@ module I18n
   def self.tc(key, params={})
     'label_with_colon'.t label: key.t(params)
   end
+
+  def self.tp(key, params={})
+    "prefix.#{key}".t(label: key.t(params))
+  end
 end
 
 class Symbol
@@ -47,7 +51,13 @@ class ActiveRecord::Base
     "models.#{model_name.to_s.underscore.gsub('/', '_').pluralize}".t
   end
 
+  def self.tp(params={})
+    key = "models.#{model_name.to_s.underscore.gsub('/', '_')}"
+    I18n.tp(key, params)
+  end
+
   def tc(params={})
     I18n.tc(self, params)
   end
+
 end
