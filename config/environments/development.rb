5402ff7845eda58d92823f04980a4d1c3c818280
abe33 <cedric.nehemie@gmail.com>
2014-12-03 15:44:54 +0100

    Allow to change the HOST in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   # Send email in development mode?
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.asset_host = 'http://localhost:3000'
+  config.action_mailer.asset_host = ENV['HOST'] || 'http://localhost:3000'
   config.action_mailer.preview_path = "spec/mailers/previews"
 
   # Adds additional error checking when serving assets at runtime.
