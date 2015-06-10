cf37c2770a0e80217a909f993eeda7813c336d9f
abe33 <cedric.nehemie@gmail.com>
2015-06-10 10:05:43 +0200

    Add micro data in breadcrumb helper

--- app/helpers/breadcrumb_helper.rb
+++ app/helpers/breadcrumb_helper.rb
@@ -30,26 +30,38 @@ module BreadcrumbHelper
     root = controller.is_admin? ? [:admin, :root] : [:root]
     # root = [:root]
 
-    content_tag :div, id: 'breadcrumb', class: 'breadcrumb' do
+    content_tag :ul, id: 'breadcrumb', class: 'breadcrumb', itemscope: true, itemtype: 'http://schema.org/BreadcrumbList' do
       txt = ([:nav] + root).join('.').t
 
       concat(content_tag(:span, capture_haml(&block), class: 'text')) if block_given?
 
-      concat(content_tag(:li) do
-        link_to(txt, root)
+      concat(content_tag(:li, itemprop: "itemListElement", itemscope: true,
+      itemtype: "http://schema.org/ListItem") do
+        content_tag(:a, href: url_for(root), itemprop: :item) do
+          concat(content_tag(:span, txt, itemprop: :name))
+          concat(tag(:meta, content: 1, itemprop: :position))
+        end
       end)
 
       (@_breadcrumb_paths || []).each.with_index do |path, i|
 
         if path[1]
-          caption = link_to(*path)
+          txt, url = path
+          caption = content_tag(:a, href: url_for(url), itemprop: :item) do
+            concat(content_tag(:span, txt, itemprop: :name))
+            concat(tag(:meta, content: i + 2, itemprop: :position))
+          end #link_to(*path)
         else
-          caption = content_tag(:a, path[0], class: 'active', href: '#')
+          caption = content_tag(:a, class: 'active', href: '#') do
+            concat(content_tag(:span, path[0], itemprop: :name))
+            concat(tag(:meta, content: i + 2, itemprop: :position))
+          end
         end
 
         li_class = path[2].delete(:class) if path[2]
 
-        concat content_tag(:li, caption, class: li_class)
+        concat content_tag(:li, caption, class: li_class, itemprop: "itemListElement", itemscope: true,
+        itemtype: "http://schema.org/ListItem")
       end
     end
   end
