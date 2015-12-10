1ca0766ba7f791a216fcf2fecd9738f7f2b5a0f3
abe33 <cedric.nehemie@gmail.com>
2015-12-10 15:39:51 +0100

    Add model annotations

--- spec/factories/bp_tests.rb
+++ spec/factories/bp_tests.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: bp_tests
+#
+#  id           :integer          not null, primary key
+#  image        :string(255)
+#  pdf          :string(255)
+#  int          :integer
+#  json         :json
+#  markdown     :text
+#  text         :text
+#  created_at   :datetime
+#  updated_at   :datetime
+#  enum         :string(255)
+#  approved_at  :datetime
+#  validated_at :datetime
+#
+
 FactoryGirl.define do
   factory :bp_test do
     image "MyString"
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,3 +1,27 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  email                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
+#  reset_password_token   :string(255)
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :inet
+#  last_sign_in_ip        :inet
+#  created_at             :datetime
+#  updated_at             :datetime
+#  first_name             :string(255)
+#  last_name              :string(255)
+#  role                   :integer
+#  avatar                 :string(255)
+#  avatar_meta            :text
+#
+
 FactoryGirl.define do
   factory :user do
     first_name { Faker::Name.first_name }
