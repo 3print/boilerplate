21db16c0b5d1d20232a5f74c246f70f37f02ddac
abe33 <cedric.nehemie@gmail.com>
2015-12-11 01:01:48 +0100

    Implement seed task based on yml files
    Files are located in the db/seeds directory, their name is the plural
    name of the resource, and their content is an array of the models’
    attributes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,23 @@
-u = User.new
-u.email = "admin@boilerplate.com"
-u.password = u.password_confirmation = "adminboilerplate"
-u.role = :admin
-u.save!
+root_path = File.dirname(__FILE__)
+seed_pattern = File.join(root_path, 'seeds', "*.yml")
+
+seed_files = Dir[seed_pattern]
+
+seed_files.each do |seed_file|
+  seeds = YAML.load_file(seed_file)
+  model_class = File.basename(seed_file, '.yml').singularize.camelize.constantize
+
+  seeds.each do |seed|
+    begin
+      model = model_class.new(seed)
+      model.save!
+    rescue => e
+      if model.present?
+        p model
+        p model.errors.messages.map {|k,v| "#{k}: #{v.to_sentence}" }.join('\n')
+      else
+        raise e
+      end
+    end
+  end
+end
