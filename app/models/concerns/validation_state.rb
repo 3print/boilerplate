dae01dfd21821ec74ede46999c4417ce0632d3a1
abe33 <cedric.nehemie@gmail.com>
2015-12-13 16:44:10 +0100

    Extract validation state in a concern

---
+++ app/models/concerns/validation_state.rb
@@ -0,0 +1,13 @@
+module ValidationState
+  extend ActiveSupport::Concern
+
+  included do
+    self.after_validation do
+      @validated = true
+    end
+  end
+
+  def was_validated?
+    @validated
+  end
+end
