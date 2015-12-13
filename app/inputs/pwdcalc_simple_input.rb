3de414d603a717a906024b1352658221b94db963
abe33 <cedric.nehemie@gmail.com>
2015-12-13 13:08:10 +0100

    Override pwdcalc input to prevent warnings

---
+++ app/inputs/pwdcalc_simple_input.rb
@@ -0,0 +1,12 @@
+# Overrides pwdcalc input to add input argument and prevent warning
+# when rendering the input
+class PwdcalcSimpleInput < SimpleForm::Inputs::Base
+  include Pwdcalc::PasswordStrength
+
+  def input(wrapper_options)
+    input_html_options[:placeholder] = placeholder_text
+    @builder.password_field(attribute_name, input_html_options) <<
+    password_strength_score <<
+    password_strength_meter
+  end
+end
