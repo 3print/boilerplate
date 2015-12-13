57467dbeba9892caa740f2cd9f792a2e7caa2964
abe33 <cedric.nehemie@gmail.com>
2015-12-13 13:07:45 +0100

    Display state icon for every field with validators

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -57,7 +57,7 @@ module SimpleForm
         end
 
         css << (options[:wrapper_error_class] || @defaults[:error_class]) if input.has_errors?
-        css << (options[:wrapper_success_class] || @defaults[:success_class]) if input.object.present? && input.object.validated? && !input.has_errors? && css.include?(:required)
+        css << (options[:wrapper_success_class] || @defaults[:success_class]) if input.object.present? && input.object.validated? && !input.has_errors? && input.has_validators?
         css << (options[:wrapper_hint_class] || @defaults[:hint_class]) if input.has_hint?
         css.compact
       end
