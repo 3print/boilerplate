a404e3ff3cc70d45d49cab0ec10dec51b52e1cc1
abe33 <cedric.nehemie@gmail.com>
2016-02-08 12:44:16 +0100

    Allow to run seeds task several times

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,15 +3,24 @@ seed_pattern = File.join(root_path, 'seeds', "*.yml")
 
 seed_files = Dir[seed_pattern]
 
+def as_query(seed)
+  seed.delete(:password)
+  seed.delete(:password_confirmation)
+end
+
 seed_files.each do |seed_file|
   seeds = YAML.load_file(seed_file)
   model_class = File.basename(seed_file, '.yml').singularize.camelize.constantize
 
   seeds.each do |seed|
     begin
-      model = model_class.new(seed)
-      model.save!
-      p "model #{model} created successfully"
+      unless model = model_class.where(as_query(seed)).first
+        model = model_class.new(seed)
+        model.save!
+        p "model #{model} created successfully"
+      else
+        p "model #{model} already existed"
+      end
     rescue => e
       if model.present?
         p model
