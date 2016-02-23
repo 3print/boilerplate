0cdc7ecbf522a239f7d47e22326be67929d61cb0
Zog <johan@about-blank.fr>
2016-02-23 12:23:42 +0100

    fix migrations

--- db/migrate/20141029135735_add_avatar_to_users.rb
+++ db/migrate/20141029135735_add_avatar_to_users.rb
@@ -2,6 +2,5 @@ class AddAvatarToUsers < ActiveRecord::Migration
   def change
     add_column :users, :avatar, :string
     add_column :users, :avatar_meta, :text
-    add_column :users, :avatar_gravity, :integer
   end
 end
