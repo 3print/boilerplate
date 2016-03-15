0690b1ecaae89593580402ed8da4fdea8949aa73
abe33 <cedric.nehemie@gmail.com>
2016-03-15 11:26:55 +0100

    :bug: Fix orderable save sequence with empty string sequence

--- app/models/concerns/orderable.rb
+++ app/models/concerns/orderable.rb
@@ -4,7 +4,9 @@ module Concerns::Orderable
   extend ActiveSupport::Concern
 
   def update_items_sequences(sequences)
-    values = sequences.map {|k,v| "(#{k},#{v['sequence']})"}
+    values = sequences.map do |k,v|
+      "(#{k},#{v['sequence'].present? ? v['sequence'] : 0})"
+    end
     query = """
     UPDATE #{self.table_name} AS s SET
       sequence = c.sequence
