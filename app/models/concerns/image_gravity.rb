f1377b8ec320e8401745f59dcaed171227eeb4ea
abe33 <cedric.nehemie@gmail.com>
2016-03-23 16:20:41 +0100

    Change check on gravity change for image field

--- app/models/concerns/image_gravity.rb
+++ app/models/concerns/image_gravity.rb
@@ -20,7 +20,7 @@ module Concerns::ImageGravity
         gravity_changed_key = :"#{key}_gravity_changed?"
         changed_key = :"#{key}_changed?"
 
-        if (respond_to?(gravity_changed_key) && send(gravity_changed_key)) &&
+        if (respond_to?(gravity_changed_key) && send(gravity_changed_key)) ||
            (respond_to?(changed_key) && !send(changed_key))
           send(key).recreate_versions!
         end
