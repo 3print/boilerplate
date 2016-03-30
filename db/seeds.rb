e7b57637e7d98724ffb7796f58109908c4a2b811
abe33 <cedric.nehemie@gmail.com>
2016-03-30 12:35:15 +0200

    Update seeds to allow custom ignored attribute list for query

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,20 +3,26 @@ seed_pattern = File.join(root_path, 'seeds', "*.yml")
 
 seed_files = Dir[seed_pattern]
 
-def as_query(seed)
+def as_query(seed, ignores=[])
   query = seed.dup
-  query.delete('role')
-  query.delete('password')
-  query.delete('password_confirmation')
+  ignores.each { |i| query.delete(i) }
   query.symbolize_keys
 end
 
 seed_files.each do |seed_file|
-  seeds = YAML.load_file(seed_file)
+  seeds_settings = YAML.load_file(seed_file).with_indifferent_access
   model_class = File.basename(seed_file, '.yml').singularize.camelize.constantize
 
+  if seeds_settings.is_a?(Array)
+    seeds = seeds_settings
+    ignores = []
+  else
+    seeds = seeds_settings[:seeds]
+    ignores = seeds_settings[:ignore_in_query]
+  end
+
   seeds.each do |seed|
-    query = as_query(seed)
+    query = as_query(seed, ignores)
     begin
       unless model = model_class.where(query).first
         model = model_class.new(seed)
