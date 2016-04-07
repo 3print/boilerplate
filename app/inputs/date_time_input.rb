bfc71eec168f6d8ef0ff087a2d6904062b88319a
abe33 <cedric.nehemie@gmail.com>
2016-04-07 15:55:03 +0200

    Make date input aware of the column type

--- app/inputs/date_time_input.rb
+++ app/inputs/date_time_input.rb
@@ -1,13 +1,18 @@
 class DateTimeInput < SimpleForm::Inputs::DateTimeInput
   def input(wrapper_options = nil)
-    if options[:type] == :datetime
+    type = options[:type] || column.type
+    if type == :datetime
       input_html_classes.unshift("datetimepicker")
+      input_html_options[:placeholder] = 'simple_form.placeholders.datetime'.t
+    elsif type == :time
+      input_html_classes.unshift("timepicker")
+      input_html_options[:placeholder] = 'simple_form.placeholders.time'.t
     else
       input_html_classes.unshift("datepicker")
+      input_html_options[:placeholder] = 'simple_form.placeholders.date'.t
     end
     options.delete(:type)
     input_html_options[:type] = 'text'
-    input_html_options[:placeholder] = 'simple_form.placeholders.datetime'.t
 
     s = "<div class='input-group'>"
     s << "<label class='input-group-addon' for='#{object_name}_#{attribute_name}'><i class='fa fa-calendar'></i></label>"
