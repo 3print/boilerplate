50df106d97f77ac8572d399b8e4f1bf3623c69ae
Cédric Néhémie <cedric.nehemie@gmail.com>
2016-10-11 15:45:49 +0200

    :bug: Fix matching integer keys in array

--- app/controllers/concerns/json_extensions.rb
+++ app/controllers/concerns/json_extensions.rb
@@ -1,7 +1,7 @@
 module JsonExtensions
   def arrayify(param_object)
     if param_object.is_a?(Hash)
-      if param_object.keys.all? {|k| k.to_s =~ /^\d$/ }
+      if param_object.keys.all? {|k| k.to_s =~ /^\d+$/ }
         param_object.keys.sort.map {|k| arrayify(param_object[k]) }
       else
         param_object.each_pair do |k,v|
