cce56139d55440f3c2f35200561bdc7fa1b8c23d
Vincent Prigent <vincent.prigent@hotmail.fr>
2016-12-27 11:36:04 +0100

    Remove store from pundit helper

--- app/helpers/pundit_helper.rb
+++ app/helpers/pundit_helper.rb
@@ -3,14 +3,6 @@ module PunditHelper
     if record.is_a?(Class)
       klass = record
       record = klass.new
-
-      if @store.present?
-        if association_columns(record, :belongs_to, :has_one).include?(:store)
-          record.store = @store
-        elsif association_columns(record, :has_and_belongs_to_many, :has_many).include?(:stores)
-          record.stores = [@store]
-        end
-      end
     end
 
     if record.present?
