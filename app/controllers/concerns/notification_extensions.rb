46f872820b60200069a010f5d42414edc12c0ea4
Vincent Prigent <vincent.prigent@hotmail.fr>
2017-01-25 17:31:42 +0100

    Show additional informations on exception for slack

---
+++ app/controllers/concerns/notification_extensions.rb
@@ -0,0 +1,15 @@
+module NotificationExtensions
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :set_notification
+
+    def set_notification
+      request.env['exception_notifier.exception_data'] = {
+        "user-agent" => request.user_agent,
+        "domain-name" => request.host,
+      }
+       # can be any key-value pairs
+    end
+  end
+end
