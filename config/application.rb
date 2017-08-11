eb7808c92f8b48c7db95104ac518baf8660e8db1
Vincent Prigent <vincent.prigent@hotmail.fr>
2017-08-11 16:19:47 +0200

    📅 Fix time mismatch

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module Boilerplate
 
     # Our default timezone
     config.time_zone = 'Paris'
+    config.active_record.default_timezone = :local
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
