82270f228a11fe59ddd46784d724905465368478
abe33 <cedric.nehemie@gmail.com>
2021-08-04 11:59:32 +0200

    :bug: Fix avatar uploader

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -37,8 +37,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
     args = args[0].call if args[0].is_a?(Proc)
 
     gravity_key = :"#{mounted_as}_gravity"
-    if self.model.respond_to?(gravity_key) && gravity = self.model.send(gravity_key).sub("#{mounted_as}_", "")
-      args[2] = "Magick::#{gravity.camelize}Gravity".constantize
+    if self.model.respond_to?(gravity_key) && gravity = self.model.send(gravity_key)
+      args[2] = "Magick::#{gravity.sub("#{mounted_as}_", "").camelize}Gravity".constantize
     end
 
     super(*args)
