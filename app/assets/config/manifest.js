95a43c7c9310f02303c20fecba900d8ecf7f6f2c
abe33 <cedric.nehemie@gmail.com>
2021-08-12 15:58:55 +0200

    Remove js from sprockets manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,3 @@
 //= link_tree ../images
-//= link_directory ../javascripts .js
-//= link_directory ../javascripts .js.jst.ejs
 //= link_directory ../stylesheets .css
-//= link_directory ../../../vendor/assets/javascripts .js
 //= link_directory ../../../vendor/assets/stylesheets .css
