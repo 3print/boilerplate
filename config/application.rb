cdb69e6a59ecb9fe206cb8a029748ae6fecdbd3b
abe33 <cedric.nehemie@gmail.com>
2021-09-22 11:28:04 +0200

    Split locales in subdirectories

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module Boilerplate
     config.active_record.default_timezone = :local
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir["#{Rails.root.to_s}/config/locales/**/*.{rb,yml}"]
     config.i18n.default_locale = :fr
 
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
