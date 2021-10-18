c1ef89d281e1d5f1ad9c3f51e06f74d9de64f36c
abe33 <cedric.nehemie@gmail.com>
2021-10-18 15:45:55 +0200

    Add comments regarding locales switcher

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,4 +1,6 @@
 class Admin::ApplicationController < ApplicationController
+  # If your project doesn't require multiple locales you can just
+  # remove the following line.
   include LocalesExtensions
 
   if Rails.env.dev?
@@ -9,6 +11,9 @@ class Admin::ApplicationController < ApplicationController
   layout 'admin'
 
   before_action :reject_unauthorized_user!
+
+  # If your project doesn't require multiple locales you can just
+  # remove the following line.
   before_action :set_locale!
 
   load_and_authorize_resource
